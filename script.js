2ab5b190ea8a5f1a33e671bc780d05d023df8cbf
Katie <katherinesarff@gmail.com>
2024-10-27 19:21:47 -0500

    added axios and API integration with user prompt

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
+function displayGeneratedRecipe(response){
+    console.log(response.data.answer);
+    new Typewriter('#recipe', {
+        strings: response.data.answer,
+        autoStart: true,
+        delay: 30,
+      });
+
+}
+
 
 function generateRecipe(event){
     event.preventDefault();
-    new Typewriter('#recipe', {
-        strings: "Italian bread sticks",
-        autoStart: true,
-      });
-  
+
+    let apiKey = "7o60e48082t80b65afac13511e68bed5";
+    let userInstructions = document.querySelector("#user-instructions")
+    let prompt = `Generate a thirty minute or less ${userInstructions.value} recipe`
+    let context = "You are a world renowned cheff.please provide a simple and concise recipe. provide recipe in basic HTML list format"
+    let apiUrl =`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
     
+    axios.get(apiUrl).then(displayGeneratedRecipe)
+  
 }
 
 let recipeForm = document.querySelector("#form-generator");
 recipeForm.addEventListener("submit", generateRecipe);
 
-
-
-new Typewriter('#typewriter', {
-  strings: ['Hello', 'World'],
-  autoStart: true,
-});
