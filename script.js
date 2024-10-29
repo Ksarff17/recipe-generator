192f2e66345138f28a16f43a0d71db8f5a941b50
Katie <katherinesarff@gmail.com>
2024-10-28 19:36:47 -0500

    improved UX

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 function displayGeneratedRecipe(response){
     console.log(response.data.answer);
     new Typewriter('#recipe', {
@@ -8,17 +10,20 @@ function displayGeneratedRecipe(response){
 
 }
 
-
 function generateRecipe(event){
     event.preventDefault();
 
-    let apiKey = "";
+    let apiKey = "7o60e48082t80b65afac13511e68bed5";
     let userInstructions = document.querySelector("#user-instructions")
     let prompt = `Generate a thirty minute or less ${userInstructions.value} recipe`
     let context = "You are a world renowned cheff.please provide a simple and concise recipe. provide recipe in basic HTML list format"
     let apiUrl =`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
     
-    axios.get(apiUrl).then(displayGeneratedRecipe)
+    axios.get(apiUrl).then(displayGeneratedRecipe);
+
+    let recipe = document.querySelector(".recipe");
+    recipe.classList.remove("hidden");
+    recipe.innerHTML = `Generating a delicious ${userInstructions.value} recipe for you...`
   
 }
 
